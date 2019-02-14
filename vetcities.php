06fbac041443dcebef67c984fb0f608957b40a21
Jeffrey Schilling <jeffreyschillingjr@gmail.com>
2019-02-14 09:58:02 -0700

    Add functionality for vetcities.php

---
+++ vetcities.php
@@ -0,0 +1,22 @@
+<?php
+  error_reporting(0); // Do not display errors
+
+  $mysqli = new mysqli('localhost','employee','123654','API'); // Connect to database
+  if ($mysqli->connect_errno) {
+      echo $mysqli->connect_errno . "\n";
+      echo "You do not have access to the database." . "\n";
+      exit;
+  }
+  $sql = "SELECT DISTINCT city AS Cities from Veternarian;";
+
+  $result=mysqli_query($mysqli,$sql);
+  while($row = mysqli_fetch_assoc($result)){
+    $test[] = $row;
+  }
+  $JSON = json_encode($test);
+  header("Content-Type: application/json; charset=UTF-8");
+  echo $JSON;
+  mysqli_free_result($result);
+  mysqli_close($mysqli);
+
+?>
